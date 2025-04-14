5d01e0ed8940f3599c597fce4bcfa35a1ceb8c79
study4cloud <study4cloud@gmail.com>
2025-04-14 20:29:32 -0300

    Implementaçao de javascript e links dinamicos

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const phoneNumber = "5511995555313";  
+    const links = document.querySelectorAll('.wa-link');
+    const titulos = document.querySelectorAll('.whatsapp-message');
+    const valorPacote1 = "20,00";
+    const valorPacote2 = "25,00";
+    
+    links.forEach((link, index) => {
+      const isPacote1 = index % 2 === 0;
+      const tituloIndex = Math.floor(index / 2);
+      const titulo = titulos[tituloIndex].textContent.trim();
+      const valor = isPacote1 ? valorPacote1 : valorPacote2;
+      const pacote = isPacote1 ? "1" : "2";
+  
+      const mensagem = `Quero o Pacote ${pacote}<br>${isPacote1 ? "5" : "10"} Simulados - R$ ${valor}`;
+      const mensagemCodificada = encodeURIComponent(mensagem.replace(/<br>/g, '\n'));
+      const mensagemWhatsapp = `Quero o Pacote ${pacote} - ${isPacote1 ? "5" : "10"} Simulados - ${titulo}`
+  
+      link.innerHTML = mensagem;
+      link.href = `https://wa.me/${phoneNumber}?text=${mensagemWhatsapp}`;
+    });
+  });
+  
