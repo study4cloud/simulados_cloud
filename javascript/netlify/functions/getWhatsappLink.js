40efd64a8337c104f4dbd11d2f1da5e116e6712c
study4cloud <study4cloud@gmail.com>
2025-04-16 02:40:29 -0300

    Adiciona função para gerar link do WhatsApp

--- javascript/netlify/functions/getWhatsappLink.js
+++ javascript/netlify/functions/getWhatsappLink.js
@@ -1,16 +1,16 @@
 exports.handler = async (event) => {
-    const phoneNumber = "5511995555313"; // coloque aqui o número SEM DDD ou com código do país, ex: 5511912345678
+    const phoneNumber = "5511995555313"; // número protegido, só fica visível no backend
   
     const pacote = event.queryStringParameters.pacote || "1";
     const titulo = event.queryStringParameters.titulo || "Simulado";
     const qtd = pacote === "1" ? "5" : "10";
-    const valor = pacote === "1" ? "20,00" : "25,00";
   
-    const mensagemWhatsapp = `Quero o Pacote ${pacote} - ${qtd} Simulados - ${titulo}`;
-    const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensagemWhatsapp)}`;
+    const mensagem = `Quero o Pacote ${pacote} - ${qtd} Simulados - ${titulo}`;
+    const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensagem)}`;
   
     return {
       statusCode: 200,
       body: JSON.stringify({ link }),
     };
   };
+  
